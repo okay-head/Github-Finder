df6ccede3edbbeb8584b893b768f5a48a8c48737
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-04-23 18:55:32 +0530

    fixed nav links

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -4,12 +4,12 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faGithub} from '@fortawesome/free-brands-svg-icons';
 
 export default function Navbar() {
+  
   return (
     <nav>
       <Link to='/' className="title">Github finder</Link>
       <Link to='/'><FontAwesomeIcon icon={faHouse} size="lg"/></Link>
-      <Link to='/'><FontAwesomeIcon icon={faGithub} size="lg" /></Link>
-      {/* <Link to='/'><FontAwesomeIcon icon="fa-brands fa-github" /></Link> */}
+      <Link onClick={()=>{window.open('https://github.com/okay-head/Github-Finder','_blank')}}><FontAwesomeIcon icon={faGithub} size="lg" /></Link>
     </nav>
   )
 }
